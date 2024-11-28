5526598157d91a969cdbf3035b7cf3494b9e3479
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2024-11-28 21:13:50 +0100

    feat: errors

--- UX/design/src/components/erroroutput/ErrorOutput.tsx
+++ UX/design/src/components/erroroutput/ErrorOutput.tsx
@@ -1,11 +1,14 @@
-
-
+import {Error} from "./error/Error";
+import './style.css'
 
 export const ErrorOutput = () => {
 
   return (
-      <section id="error-output">
-
+      <section id="error-output" className="flex justify-center">
+        <div className="errors w-11/12 flex flex-col my-4 gap-4">
+          <Error></Error>
+          <Error></Error>
+        </div>
       </section>
   )
 }
---
+++ UX/design/src/components/erroroutput/error/Error.tsx
@@ -0,0 +1,33 @@
+import './style.css'
+
+// @ts-ignore
+import arrowRightIcon from '../../../assets/icons/arrow_right.png'
+import {useState} from "react";
+
+
+interface ErrorProps {
+  message: string;
+}
+
+
+export const Error = () => {
+
+  const [expand, setExpand] = useState(false)
+
+  const handleClick = (): void => {
+    setExpand(!expand)
+  }
+
+  return (
+      <div className="error-container" onClick={handleClick}>
+        <div className="w-11/12">
+          <h3 className="error-title">Error</h3>
+          <p className={expand ? "error-text-expanded" : "error-text"}>
+            moderatius ullamcorper partiendo esse mollis oratio mus ornare suscipit noster mediocritatem tation tempor
+            adipiscing enim ludus dicam nobis mediocritatem hinc
+          </p>
+        </div>
+        <img src={arrowRightIcon} className={expand ?"rotated" : ""} alt="arrow-right" />
+      </div>
+  )
+}
